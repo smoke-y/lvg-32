8dd5522dca2e2b19a5749e7c40c9d54ac1d62225
smoke-y <SnowSneeze@protonmail.com>
2025-06-25 13:43:38 +0530

    xor problem solved by out tpu

--- script/xor.py
+++ script/xor.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.optim as optim
 import struct
 
+torch.manual_seed(0)
+
 def float_to_hex(f):
     hex_val = struct.unpack('<I', struct.pack('<f', f))[0]
     return f"32'h{hex_val:08x}"
@@ -10,11 +12,13 @@ def float_to_hex(f):
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.l1 = nn.Linear(2, 1)
+        self.l1 = nn.Linear(2, 4)
+        self.l2 = nn.Linear(4, 1)
         self.act = nn.ReLU(inplace=True)
 
     def forward(self, x):
-        return self.act(self.l1(x))
+        x = self.act(self.l1(x))
+        return self.l2(x)
 
 dataSet = [
         (0,0),
@@ -25,12 +29,12 @@ dataSet = [
 
 model = Net()
 criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters())
+optimizer = optim.Adam(model.parameters(), lr=0.1)
 
-for epoch in range(500):
+for epoch in range(900):
     for data in dataSet:
         pred = model(torch.tensor(data, dtype=torch.float).reshape(1, -1))
-        truth = torch.tensor(data[0] & data[1], dtype=torch.float).reshape(1, -1)
+        truth = torch.tensor(data[0] ^ data[1], dtype=torch.float).reshape(1, -1)
         loss = criterion(pred, truth)
         optimizer.zero_grad()
         loss.backward()
@@ -38,7 +42,22 @@ for epoch in range(500):
 
 for data in dataSet:
     pred = model(torch.tensor(data, dtype=torch.float))
-    print(f"{data[0]} & {data[1]} = {pred.detach().numpy()}")
+    print(f"{data[0]} ^ {data[1]} = {pred.detach().numpy()}")
 
+print("WEIGHT_1")
 for row in model.l1.weight.data.T:
     for val in row: print(float_to_hex(val), end=" ")
+    print()
+print("BIAS_1")
+for row in model.l1.bias.data:
+    print(float_to_hex(row), end=" ")
+    print()
+print()
+print("WEIGHT_2")
+for row in model.l2.weight.data.T:
+    for val in row: print(float_to_hex(val), end=" ")
+    print()
+print("BIAS_2")
+for row in model.l2.bias.data:
+    print(float_to_hex(row), end=" ")
+    print()
